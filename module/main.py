267d4820bfd4095495aec5902f68082acf246b0e
YXTang03 <2244656809@qq.com>
2025-08-01 16:08:36 +0800

    Progress bar is now available.

--- module/main.py
+++ module/main.py
@@ -1,19 +1,33 @@
 from pymol import cmd
 from .utils import track_and_log
 from .utils import setup_full_logger
+from tqdm import tqdm
 
 def main(
         work_dir:str,
-        full_log_dir:str = 'E:/Coding/jupyter_root/ynby/pj3/repo/test/full_log.txt',
-        rcsb_pdb_ids:str =  'E:/Coding/jupyter_root/ynby/pj3/repo/test/test.txt', 
-        output_dir:str = 'E:/Coding/jupyter_root/ynby/pj3/repo/test/log.txt', 
+        rcsb_pdb_ids:str ,
+        full_log_dir:str ,
+        output_dir:str , 
+        output_residual_index:bool = True
         
 ):
+    
     if full_log_dir:
         full_logger = setup_full_logger(full_log_dir)
     cmd.cd(work_dir)
     with open(rcsb_pdb_ids, 'r') as f:
         content = f.read()
         content = content.split(',')
-        for id in content: 
-                track_and_log(id, output_dir,full_log_dir=full_logger)
+        pbar = tqdm(
+            content, 
+            colour='CYAN',
+            smoothing= 1.0
+        )
+        for id in pbar: 
+            pbar.set_description(f"Processing PDB: {id}" )
+            track_and_log(
+                id, 
+                output_dir,
+                full_log_dir=full_logger, 
+                output_residual_index = output_residual_index
+            )
